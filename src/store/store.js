ee7b36c9dcc5d9e0acd3f0f5d99b04d7b3c6739a
NKDuy <nkduy.dev@gmail.com>
2026-08-10 14:17:08 +0700

    Getters

--- src/store/store.js
+++ src/store/store.js
@@ -11,5 +11,16 @@ export const store = new Kdux.Store({
         { name: 'Green Shells', price: 60 },
         { name: 'Red Sheels', price: 80 }
       ]
+  },
+  getters: {
+    saleProducts: state => {
+      var saleProducts = state.products.map(product => {
+        return {
+          name: '**' + product.name + '**',
+          price: product.price / 2
+        }
+      });
+      return saleProducts;
+    }
   }
 })
